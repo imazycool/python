98bdb541cf7b67a6cf56e0bbd2a01adee199ba00
imazy <imazycool@gmail.com>
2025-07-05 01:12:44 +0530

    python codes start

---
+++ slicing.py
@@ -0,0 +1,11 @@
+#program to reverse the string using slicing and loop 
+class RevString:
+    
+    def rev_string(self, input):
+        print(input[::-1])
+    
+    def __init__(self, input):
+        self.rev_string(input)
+        
+
+RevString("ajay") 
